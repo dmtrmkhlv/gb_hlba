d1eaf4d7933e8e8033aea6878cbe178a6ba03aa7
Dmitry Mikhailov <43272273+dmtrmkhlv@users.noreply.github.com>
2022-12-01 12:45:41 +0000

    task2

--- hw2/gb-app/apps/web/src/app/news/news.tsx
+++ hw2/gb-app/apps/web/src/app/news/news.tsx
@@ -1,42 +1,53 @@
 import './news.module.scss';
 import { useEffect, useState } from 'react';
+import getData from '../utils/getData';
 
 /* eslint-disable-next-line */
 export interface NewsProps {}
 export interface PeaceOfNews {
-  id: number,
-  title: string,
-  description: string,
-  createdAt: number
+  id: number;
+  title: string;
+  description: string;
+  createdAt: number;
+  body?: string;
 }
 
 export function News(props: NewsProps) {
   const [news, setNews] = useState([] as PeaceOfNews[]);
-  const sortNews = (news: PeaceOfNews[]) => {
-    return news.sort((a, b) => a.createdAt - b.createdAt)
-  }
+  const sortNews = (news: PeaceOfNews[] | unknown) => {
+    if (news instanceof Array) {
+      return news?.sort((a, b) => a.createdAt - b.createdAt);
+    }
+    return [];
+  };
 
   useEffect(() => {
-    fetch('http://localhost:3333/api/news')
-      .then(response => response.json())
-      .then(news => {
-        const sortedNews = sortNews(news);
+    // fetch('http://localhost:3333/api/news')
+    //   .then(response => response.json())
+    //   .then(news => {
+    //     const sortedNews = sortNews(news);
+    //     setNews(sortedNews);
+    //   })
 
-        setNews(sortedNews);
-      })
+    getData('http://localhost:3333/api/news').then((news) => {
+      const sortedNews = sortNews(news);
+      setNews(sortedNews);
+    });
   }, []);
 
   return (
     <div>
       <h1>Последние новости</h1>
       <ul>
-      {news.map(peaceOfNews => {
-        return <li key={peaceOfNews.id}>
-          <h2>{peaceOfNews.title}</h2>
-          <p>{peaceOfNews.description}</p>
-          <hr/>
-        </li>
-      })}
+        {news.map((peaceOfNews) => {
+          return (
+            <li key={peaceOfNews.id}>
+              <h2>{peaceOfNews.title}</h2>
+              <p>{peaceOfNews.description}</p>
+              <hr />
+            </li>
+          );
+        })}
       </ul>
     </div>
   );
