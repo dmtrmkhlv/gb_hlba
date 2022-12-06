776af0d8e0026869922007cd3ba8ec16a5d1e918
Dmitry <app@dmtrmkhlv.ru>
2022-12-06 10:27:47 +0300

    hw4

--- hw2/gb-app/apps/api/src/app/app.module.ts
+++ hw2/gb-app/apps/api/src/app/app.module.ts
@@ -3,9 +3,18 @@ import { CacheModule, Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { NewsModule } from './news/news.module';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 
 @Module({
-  imports: [CacheModule.register({ isGlobal: true }), NewsModule],
+  imports: [
+    CacheModule.register({ isGlobal: true }),
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '../web'),
+    }),
+    NewsModule,
+  ],
+  // imports: [NewsModule],
   controllers: [AppController],
   providers: [AppService],
 })
