b1f4caaac1cd7bca35fd01b8e2ed024d752a642e
dmtrmkhlv <app@dmtrmkhlv.ru>
2022-12-19 20:20:53 +0300

    task1

--- hw4-2/apps/api/src/app/news/news.controller.ts
+++ hw4-2/apps/api/src/app/news/news.controller.ts
@@ -2,6 +2,9 @@ import { Body, Controller, Get, Header, Post } from '@nestjs/common';
 
 import { IsNotEmpty } from 'class-validator';
 
+import Redis from 'ioredis';
+const redis = new Redis();
+
 export class CreateNewsDto {
   @IsNotEmpty()
   title: string;
@@ -14,30 +17,65 @@ export class CreateNewsDto {
 export class NewsController {
   @Get()
   async getNews() {
-    return new Promise(resolve => {
+    return new Promise((resolve) => {
       const news = Object.keys([...Array(20)])
-        .map(key => Number(key) + 1)
-        .map(n => ({
+        .map((key) => Number(key) + 1)
+        .map((n) => ({
           id: n,
           title: `Важная новость ${n}`,
-          description: (rand => ([...Array(rand(1000))].map(() => rand(10**16).toString(36).substring(rand(10))).join(' ')))(max => Math.ceil(Math.random() * max)),
-          createdAt: Date.now()
-        }))
+          description: ((rand) =>
+            [...Array(rand(1000))]
+              .map(() =>
+                rand(10 ** 16)
+                  .toString(36)
+                  .substring(rand(10))
+              )
+              .join(' '))((max) => Math.ceil(Math.random() * max)),
+          createdAt: Date.now(),
+        }));
+
+      news.forEach((item) => {
+        redis.hset(
+          `news`,
+          `news:${item['id']}`,
+          JSON.stringify({
+            id: item['id'],
+            title: `Важная новость ${item['id']}`,
+            description: ((rand) =>
+              [...Array(rand(1000))]
+                .map(() =>
+                  rand(10 ** 16)
+                    .toString(36)
+                    .substring(rand(10))
+                )
+                .join(' '))((max) => Math.ceil(Math.random() * max)),
+            createdAt: Date.now(),
+          })
+        );
+        // redis.hset(`news:${item['id']}`, "id", item['id']);
+        // redis.hset(`news:${item['id']}`, "title", `Важная новость ${n}`)
+        // redis.hset(`news:${item['id']}`, "description", (rand => ([...Array(rand(1000))].map(() => rand(10**16).toString(36).substring(rand(10))).join(' ')))(max => Math.ceil(Math.random() * max)))
+        // redis.hset(`news:${item['id']}`, "createdAt", Date.now())
+      });
 
       setTimeout(() => {
-        resolve(news);
-      }, 100)
+        redis.hgetall('news', function (err, obj) {
+          // console.dir(obj);
+          return resolve(obj);
+        });
+        // resolve(news);
+      }, 100);
     });
   }
 
   @Post()
   @Header('Cache-Control', 'none')
   create(@Body() peaceOfNews: CreateNewsDto) {
-    return new Promise(resolve => {
+    return new Promise((resolve) => {
       setTimeout(() => {
         console.log('Новость успешно создана', peaceOfNews);
         resolve({ id: Math.ceil(Math.random() * 1000), ...peaceOfNews });
-      }, 100)
+      }, 100);
     });
   }
 }
